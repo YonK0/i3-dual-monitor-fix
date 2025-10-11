e376068ae9394b14c64c52b787702ad74c013452
KTHIRI Omar <omar.kthiri@sagemcom.com>
2025-10-11 19:55:50 +0100

    Enhance the script

--- fix_monitor.sh
+++ fix_monitor.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-mainscreen="eDP"
-extendscreen="HDMI-A-0"
+main=$(xrandr | grep " connected" | cut -d " " -f 1 | grep -i edp)
+extend=$(xrandr | grep " connected" | cut -d " " -f 1 | grep -i hdmi)
 
-screens=$(xrandr | grep " connected" | cut -d " " -f 1)
-
-if echo "$screens" | grep -q "$extendscreen"; then
-  echo "[+] Found '$extendscreen', let's do the work"
+if [[ -n "$extend" ]]; then
+  echo "[+] Found '$extend', let's do the work"
 else
-  echo "[-] '$extendscreen' not found, Back to default"
-  xrandr --output HDMI-A-0 --off
-  xrandr --output eDP --auto
+  echo "[-] No second monitor is found, Back to default"
+  xrandr --auto
+  xrandr --output $main --auto
   exit 1
 fi
 
-xrandr --output HDMI-A-0 --off
-sleep 1
-xrandr --output eDP --off
-sleep 1
-xrandr --output eDP --mode 2560x1440 --pos 0x0 --scale 1x1 --primary
-#xrandr --output HDMI-A-0 --mode 2560x1440 --pos 2560x0 --scale 1x1
-xrandr --newmode "1920x1080_144.00" 414.50 1920 2072 2280 2640 1080 1081 1084 1148 -HSync +Vsync
-xrandr --addmode HDMI-A-0 "1920x1080_144.00"
-xrandr --output HDMI-A-0 --mode "1920x1080_144.00" --pos 2560x0 --scale 1.5x1.5
+xrandr --auto
+xrandr --output $extend --mode 1920x1080 --right-of $main
+echo "[*] Done."
